b7daec8d298e35fbe1ca37013c6a0b13aa342696
Anthony Ruffin <aruffincs@gmail.com>
2021-05-20 22:07:53 -0400

    added event listener to connect search button

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello World')
+// Remove default form behavior
+const form = document.querySelector('form')
+
+document.addEventListener('DOMContentLoaded', function() {
+  form.addEventListener('submit', function(event) {
+    event.preventDefault()
+    alert('button pressed')
+  })
+})
