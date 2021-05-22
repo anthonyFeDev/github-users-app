fc6bc73ece00aab4e0570f35401cad34e961ddaf
Anthony Ruffin <aruffincs@gmail.com>
2021-05-21 23:46:18 -0400

    added axios and .gitignore

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+import axios from 'axios'
+// Github api url: https://api.github.com/users
+
 document.addEventListener('DOMContentLoaded', function() {
   const form = document.querySelector('form')
 
   form.addEventListener('submit', function(event) {
     event.preventDefault()
     const userName = document.querySelector('input').value
-    alert(userName)
+    const response = await axios.get(`https://api.github.com/users/${userName}`)
+    console.log(response.data)
   })
 })
