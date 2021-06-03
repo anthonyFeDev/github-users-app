d028496b36077e965ae00dbb12f6e2db7aecff53
Anthony Ruffin <aruffincs@gmail.com>
2021-06-03 14:27:38 -0400

    updates

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 import axios from 'axios'
 // Github api url: https://api.github.com/users
 
-const card = createCard(response.data)
 
 function createCard(data) {
-  `<div>
-    test
-  </div>`
+  console.log(data)
+  // `<div>
+  // ${data}
+  // </div>`
 }
 
 const searchResults = document.querySelector('.search-results')
-searchResults.insertAdjacentHTML('beforeend', card);
+// searchResults.insertAdjacentHTML('beforeend', card);
 
 document.addEventListener('DOMContentLoaded', function() {
   const form = document.querySelector('form')
-
-  form.addEventListener('submit', function(event) {
+  
+  form.addEventListener('submit', async function(event) {
     event.preventDefault()
     const userName = document.querySelector('input').value
     const response = await axios.get(`https://api.github.com/users/${userName}`)
+    // console.log(response)
+    const card = createCard(response.data)
+    return response
   })
 })
